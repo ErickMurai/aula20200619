2c9c316d5795d2a75477b472c322a10c23f6c1c5
Erick <erickmurai1@hotmail.com>
2020-06-20 23:36:59 -0300

    first commit

---
+++ src/classe/Automovel.java
@@ -0,0 +1,20 @@
+package classe;
+
+public class Automovel {
+	
+	private int portencia = 3;
+	
+	public void imprimeDados() {
+		System.out.println("Potencia = " + potencia); } }
+
+	public class Carro extends Automovel { 
+		private int consumo = 1; 
+		public void imprimeDados() {
+			super.imprimeDados(); 
+	}
+
+}
+	
+	
+	
+	/// Superclasse é a classe existente a partir da qual novas classes são derivadas enquanto a Subclasse é a nova classe que herda as propriedades e os métodos da Superclasse.
